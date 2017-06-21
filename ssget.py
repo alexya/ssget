8e81f979fe241d1187a0ddf9e0f6d003c48f2ab3
alexya <alexya@outlook.com>
2017-06-21 21:56:32 +0800

    write back proxies' information to config file

--- ssget.py
+++ ssget.py
@@ -29,53 +29,39 @@ def get_proxy(class_def_name):
             proxy.Name = x['id']
 
         hh = link.find_all("h4")
-        proxy.IPAddress = re.split(":|\xef\xbc\x9a", hh[0].text.encode('UTF-8'))[1]
-        proxy.Port = re.split(":|\xef\xbc\x9a", hh[1].text.encode('UTF-8'))[1]
-        proxy.Password = re.split(":|\xef\xbc\x9a", hh[2].text.encode('UTF-8'))[1]
-        proxy.Method = re.split(":|\xef\xbc\x9a", hh[3].text.encode('UTF-8'))[1]
+        proxy.IPAddress = re.split(":|\xef\xbc\x9a", hh[0].text.encode('UTF-8'))[1].strip()
+        proxy.Port = re.split(":|\xef\xbc\x9a", hh[1].text.encode('UTF-8'))[1].strip()
+        proxy.Password = re.split(":|\xef\xbc\x9a", hh[2].text.encode('UTF-8'))[1].strip()
+        proxy.Method = re.split(":|\xef\xbc\x9a", hh[3].text.encode('UTF-8'))[1].strip()
         proxies.append(proxy)
 
-        # print(proxy.Name)
-        # print(proxy.IPAddress)
-        # print(proxy.Port)
-        # print(proxy.Method)
-
 def parse_config(file_name):
+    # ssget folder will be put under the shadowsocksR 
+    # so, here we need to goto its parent folder and pick up config file
+
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    path_file_name = os.path.join(dir_path, file_name)
-    print(file_name)
+    parent_path = os.path.abspath(os.path.join(dir_path, os.pardir))
+    path_file_name = os.path.join(parent_path, file_name)
     print(path_file_name)
 
     with open(path_file_name) as jsonFile:
-        old_data = json.load(jsonFile)
-
-    print(old_data)
-    # old_data = json.loads(open(path_file_name, 'r').read())
-    # # print(old_data['configs'][0]['remarks'])
-    # # print(old_data['configs'][0]['server'])
-    # # print(old_data['configs'][0]['server_port'])
-    # # print(old_data['configs'][0]['password'])
-    # # print(old_data['configs'][0]['method'])
+        json_data = json.load(jsonFile)
 
     configs = []
     for p in proxies:
         data = {}
         data['remarks'] = p.Name
         data['server'] = p.IPAddress
-        data['server_port'] = p.Port
+        data['server_port'] = int(p.Port)
         data['password'] = p.Password
         data['method'] = p.Method
         data['auth'] = False
         configs.append(data)
+    json_data['configs'] = configs
 
-    config_json_data = json.dumps(configs)
-    print(config_json_data)
-
-    old_data['configs'] = config_json_data
-    #print(old_data)
-
-    with open('tt.json', 'w') as jsonFile:
-        json.dump(old_data, jsonFile)
+    with open(path_file_name, 'w') as jsonFile:
+        ss = json.dumps(json_data, sort_keys = True, indent = 4, separators = (',', ': '), encoding = "utf-8", ensure_ascii = True)
+        jsonFile.write(ss)
 
 if __name__ == "__main__":
     get_proxy("col-sm-6 col-md-4 col-lg-4 us")
