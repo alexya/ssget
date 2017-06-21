84d457e759517e5edbd2b86f1c9ed52727ba9b7e
Zhenxing Yang <Zhenxing.Yang@autodesk.com>
2017-06-21 18:18:53 +0800

    read data from config.json file

--- ssget.py
+++ ssget.py
@@ -1,7 +1,8 @@
 import urllib2
+import os 
 import re
+import json
 from bs4 import BeautifulSoup
-
 
 class ProxyData:
     def __init__(self):
@@ -11,15 +12,14 @@ class ProxyData:
         self.Password = ''
         self.Method = ''
 
-#url = "http://python.org"
 url = "http://ss.ishadowx.com/"
+config_file = "gui-config.json"
 
 response = urllib2.urlopen(url)
 html_doc = response.read()
 soup = BeautifulSoup(html_doc, 'html.parser')
 proxies = []
 
-# col-sm-6 col-md-4 col-lg-4 us
 def get_proxy(class_def_name):
     for link in soup.find_all("div",class_=class_def_name):
         proxy = ProxyData()
@@ -33,27 +33,55 @@ def get_proxy(class_def_name):
         proxy.Port = re.split(":|\xef\xbc\x9a", hh[1].text.encode('UTF-8'))[1]
         proxy.Password = re.split(":|\xef\xbc\x9a", hh[2].text.encode('UTF-8'))[1]
         proxy.Method = re.split(":|\xef\xbc\x9a", hh[3].text.encode('UTF-8'))[1]
-
-        print(proxy.Name)
-        print(proxy.IPAddress)
-        print(proxy.Port)
-        print(proxy.Method)
-
         proxies.append(proxy)
 
-        print
+        # print(proxy.Name)
+        # print(proxy.IPAddress)
+        # print(proxy.Port)
+        # print(proxy.Method)
 
-# for link in soup.find_all("div",class_="col-sm-6 col-md-4 col-lg-4 jp"):
-#     #print link
-#     print
+def parse_config(file_name):
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    path_file_name = os.path.join(dir_path, file_name)
+    print(file_name)
+    print(path_file_name)
 
-# for link in soup.find_all("div",class_="col-sm-6 col-md-4 col-lg-4 ssr"):
-#     #print link
-#     print
+    with open(path_file_name) as jsonFile:
+        old_data = json.load(jsonFile)
+
+    print(old_data)
+    # old_data = json.loads(open(path_file_name, 'r').read())
+    # # print(old_data['configs'][0]['remarks'])
+    # # print(old_data['configs'][0]['server'])
+    # # print(old_data['configs'][0]['server_port'])
+    # # print(old_data['configs'][0]['password'])
+    # # print(old_data['configs'][0]['method'])
+
+    configs = []
+    for p in proxies:
+        data = {}
+        data['remarks'] = p.Name
+        data['server'] = p.IPAddress
+        data['server_port'] = p.Port
+        data['password'] = p.Password
+        data['method'] = p.Method
+        data['auth'] = False
+        configs.append(data)
+
+    config_json_data = json.dumps(configs)
+    print(config_json_data)
+
+    old_data['configs'] = config_json_data
+    #print(old_data)
+
+    with open('tt.json', 'w') as jsonFile:
+        json.dump(old_data, jsonFile)
 
 if __name__ == "__main__":
     get_proxy("col-sm-6 col-md-4 col-lg-4 us")
     get_proxy("col-sm-6 col-md-4 col-lg-4 jp")
     get_proxy("col-sm-6 col-md-4 col-lg-4 ssr")
 
-    print("The objects account is {}".format(len(proxies)))
+    print("The ss proxy account is {}".format(len(proxies)))
+
+    parse_config(config_file)
